39a28dcebb6d302c057e81fbd329efea431f173a
mehdiaix <mehdibougattaya@gmail.com>
2018-09-13 15:37:14 +0200

    ajout de la balise html a intérpreter

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# javascript
+<h1> javascript </h1>
+<hr>
+<p> ceci est un répertoire où je vais recenser tous mes développements fait
+ avec Javascript pour suivre ma progression!</p>
--- fetch.js
+++ fetch.js
@@ -4,9 +4,9 @@ const getUsers = async function () {
     try{
         //je veut que tu attendent les données et que tu fini de les parser 
         let response = await fetch('https://jsonplaceholder.typicode.com/users')
-        //je fait une condition pour etre sur que sa marche car ma methode fetch ne me dit pas si il y'a une erreur 
+        //je fait une condition pour etre sur que sa marche car ma méthode fetch ne me dit pas si il y'a une erreur
         if(response.ok) {
-            //une fois qu'on a la reponse on fait un 
+            //une fois qu'on a la response on fait un
             let data = await response.json()
             // et on attend la conversion en object 
             // et lorsque j'ai les données je peut faire un console log de data 
@@ -18,20 +18,21 @@ const getUsers = async function () {
             console.log(e)
             }
         }
-//ici je lance un apel a getUsers pour m'envoyer les utilisateur
+//ici je lance un appel a getUsers pour m'envoyer les utilisateur
 getUsers()
 
+
 const insertPost = async function (data) {//parametre data
-    let response = await fetch('https://jsonplaceholder.typicode.com/posts',//en second paramétre je vais lui passer notre object
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts',//en second paramètre je vais lui passer notre object
     // et notre méthode
     {
         method:'POST',//la méthode
-        headers: {//comment je vais lui divulger les données et saura comment les données sont envoyer 
+        headers: {//comment je vais lui divulguer les données et saura comment les données sont envoyer
             'Content-Type' : 'application/json'
         },
-        body: JSON.stringify(data)//pour les envoyer sous forme de caractére
+        body: JSON.stringify(data)//pour les envoyer sous forme de caractère
     })
-    //ici j'ai récuperer ce que j'ai envoyer 
+    //ici j'ai récupérer ce que j'ai envoyer
     let responseData = await response.json()
     //je peu aussi bien faire responseData.id ...
     console.log(responseData)
